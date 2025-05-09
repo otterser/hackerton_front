ae7d3efa8872e972f1820119ba013f984a2a87fe
Dies0101 <kaillie0908@gmail.com>
2025-05-10 02:26:13 +0900

    일단 거진 다한 수정본

--- hackerton_front/src/pages/MainPage.js
+++ hackerton_front/src/pages/MainPage.js
@@ -18,16 +18,22 @@ export default function MainPage() {
           <h2 className="sub-title">알아보기!</h2>
         </div>
       </div>
-      <img 
-        src="https://cdn.pixabay.com/photo/2024/06/01/02/40/toy-8801382_1280.png" 
-        alt="곰돌이 이미지" 
-        className="title-image"
-      />
+      <div className="image-container">
+        <div className="speech-bubble">재밌어요!</div>
+        <img 
+          src="https://cdn.pixabay.com/photo/2024/06/01/02/40/toy-8801382_1280.png" 
+          alt="곰돌이 이미지" 
+          className="title-image"
+        />
+      </div>
       <div className="content-container">
         <div className="memo-container">
           <div className="memo-paper">
             <p className="memo-text">다른 심리 검사와 달리 쉽고 간단하다!!</p>
           </div>
+        </div>
+        <div className="info-box">
+          <p>3가지 주제에서 각 9문항, 총 27문항의 심리 검사로 나의 심리 MBTI를 알아볼수 있어요.</p>
         </div>
       </div>
       <button onClick={handleStart} className="startBtn">

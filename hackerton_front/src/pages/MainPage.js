41a98eb96d1ed140d07d92e4c23f5c2419e4a538
Dies0101 <kaillie0908@gmail.com>
2025-05-10 02:14:07 +0900

    수정중

--- hackerton_front/src/pages/MainPage.js
+++ hackerton_front/src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export default function StartPage() {
+export default function MainPage() {
   const navigate = useNavigate();
 
   const handleStart = () => {
@@ -11,16 +11,24 @@ export default function StartPage() {
   return (
     <div className="main">
       <div className="title-container">
-        <h1 className="title">나의 심리 MBTI</h1>
-        <h2 className="sub-title">알아보기!</h2>
+        <div className="title-wrapper">
+          <h1 className="title">나의 심리 MBTI</h1>
+        </div>
+        <div className="subtitle-wrapper">
+          <h2 className="sub-title">알아보기!</h2>
+        </div>
       </div>
+      <img 
+        src="https://cdn.pixabay.com/photo/2024/06/01/02/40/toy-8801382_1280.png" 
+        alt="곰돌이 이미지" 
+        className="title-image"
+      />
       <div className="content-container">
-        <p className="description">다른 심리 검사와 달리 쉽고 간단하다</p>
-        <img 
-          src="https://cdn.pixabay.com/photo/2016/03/31/19/13/memo-1293086_1280.png" 
-          alt="메모장 이미지" 
-          className="memo-image"
-        />
+        <div className="memo-container">
+          <div className="memo-paper">
+            <p className="memo-text">다른 심리 검사와 달리 쉽고 간단하다!!</p>
+          </div>
+        </div>
       </div>
       <button onClick={handleStart} className="startBtn">
         시작하기
